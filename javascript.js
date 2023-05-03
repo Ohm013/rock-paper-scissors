2c658544fb54b898289ded081583ddbd3ee3eece
Ohm Patel <60417541+Ohm013@users.noreply.github.com>
2023-05-03 11:42:53 -0500

    added if statements to playSelection and tried to make computerchoice answer a variable type so the playROund function can recognize it, but failed.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,21 @@
 
-function playerSelection () {
+
+
+function playerSelection (option) {
     let input = prompt("Rock, Paper, or Scissors").toLowerCase();
+    if (input === "rock") {
+        option = "rock" ;
+    }else if (input === "paper"){
+        option = "paper" ;
+    }else if (input === "scissors") {
+        option = "scissors";
+    }else {
+        alert("Choose from Rock, Paper, or Scissors")
+    }
+    
+
     alert(`You chose ${input}!`);
+    
 }
 
 playerSelection();
@@ -11,12 +25,16 @@ playerSelection();
 function getComputerChoice (choice) {
     choice = Math.floor(Math.random() *100);
         if (choice <= 33) {
+            choice = "Rock";
             alert("Rock");
         }else if (choice > 33 && choice <= 66) {
+            choice = "Paper"
             alert ("Paper") ;
         }else {
+            choice = "Scissors"
             alert ("Scissors");
         } 
+    
 }
 
 getComputerChoice();
@@ -24,8 +42,10 @@ getComputerChoice();
 
 
 function playRound (playerSelection, getComputerChoice ) {
-    if (playerSelection == "rock" && getComputerChoice == "Paper") {
-        alert ("You lose! Paper beats rock");
+    if (playerSelection === getComputerChoice ) {
+        alert ("Try again! It's a tie");
+    }else if  (playerSelection == "rock" && getComputerChoice == "Paper") {
+            alert ("You lose! Paper beats rock");
     }else if (playerSelection == "paper" && getComputerChoice == "Rock") {
         alert ("You win! Paper beats Rock");
     }else if (playerSelection == "rock" && getComputerChoice == "Scissors") {
@@ -36,9 +56,11 @@ function playRound (playerSelection, getComputerChoice ) {
         alert ("You lose! Scissors beats Paper");
     }else if (playerSelection == "scissors" && getComputerChoice == "Paper") {
         alert ("You win! Scissors beats Paper");
-    }else {
-        alert ("Try again! It's a tie") ;
+    }else{ 
+        playRound();
     }
+
+    return ;
 }
     
 playRound();
