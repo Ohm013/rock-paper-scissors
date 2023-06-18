2ad1d4f94cf24eb781fe6a300328e64ab57455d7
Ohm Patel <60417541+Ohm013@users.noreply.github.com>
2023-06-18 16:23:13 -0500

    refined playround function, still can't get querySelector to work

--- javascript.js
+++ javascript.js
@@ -1,17 +1,15 @@
-const buttons = document.querySelectorAll("button"); 
+const buttons = document.querySelectorAll('button'); 
 
-
-buttons.forEach((button) => {
-        button.addEventListener('click', function(){ 
-        playRound(button.value);  
+buttons.forEach(button => {
+    button.AddEventListener('click', function () { 
+        playRound(button.value); 
 
     });    
+
 }); 
 
-function playerSelection () {}
-
-
-
+let playerScore = 0 ;
+let computerScore = 0 ;
 
 function getComputerChoice () {
     let choice = Math.floor(Math.random() *100);
@@ -22,87 +20,65 @@ function getComputerChoice () {
             return "Paper" ;
         }else {
             return "Scissors" ;
-           
-        } 
-    
-    
+    } 
 }   
 
-
-
-let playerScore = 0 ;
-let computerScore = 0 ;
-
-
-
-function playRound (playerChoice) {
-
-
-
-    playerChoice = playerSelection ; 
+function playRound (playerSelection) {
    
     let computerSelection = getComputerChoice();
         alert(`Computer chose ${computerSelection}`);  
 
 
-    if ((playerChoice === "Rock") && (computerSelection === "Paper" )) {
-        alert ("You lose! Paper beats rock"); 
+    if ((playerSelection === "Rock" && computerSelection === "Paper" ) ||
+       (playerSelection === "Scissors" && computerSelection === "Rock") ||
+       (playerSelection === "Paper" && computerSelection === "Scissors")) {
+
+        alert (`You lose! ${computerSelection} beats ${playerSelection}`); 
         ++computerScore;
         
-    }else if (playerChoice === "Paper" && computerSelection === "Rock") {
-        alert ("You win! Paper beats Rock");
+    }
+    else if ((playerSelection === "Paper" && computerSelection === "Rock") ||
+            (playerSelection === "Rock" && computerSelection === "Scissors") ||
+            (playerSelection === "Scissors" && computerSelection === "Paper")) {
+       
+        alert (`You win! ${playerSelection} beats ${computerSelection}`);
         ++playerScore;
 
-    }else if (playerChoice === "Rock" && computerSelection === "Scissors") {
-        alert ("You win! Rock beats Scissors");
-        ++playerScore;
-
-    }else if (playerChoice === "Scissors" && computerSelection === "Rock") {
-        alert ("You lose! Rock beats Scissors");
-        ++computerScore;
-
-    }else if (playerChoice === "Paper" && computerSelection === "Scissors") {
-        alert ("You lose! Scissors beats Paper");
-        ++computerScore;
-
-    }else if (playerChoice === "Scissors" && computerSelection === "Paper") {
-        alert ("You win! Scissors beats Paper");
-        ++playerScore;
-
-    }else{ 
+    }
+    else{
         alert ("Try again! It's a tie");
     }
 
 }
 
-// function game () {
+//function game () {
 
-   // playRound() ;
-     //   alert (`The score is  ${playerScore} - ${computerScore} `) ;
+  //  playRound() ;
+    //    alert (`The score is  ${playerScore} - ${computerScore} `) ;
+    
+    //playRound() ;
+      // alert (`The score is  ${playerScore} - ${computerScore} `) ;
     
     //playRound() ;
       //  alert (`The score is  ${playerScore} - ${computerScore} `) ;
-    
+
     //playRound() ;
       //  alert (`The score is  ${playerScore} - ${computerScore} `) ;
 
-   // playRound() ;
-     //   alert (`The score is  ${playerScore} - ${computerScore} `) ;
-
     //playRound() ;
-   //     alert (`The score is  ${playerScore} - ${computerScore} `) ; 
+      //  alert (`The score is  ${playerScore} - ${computerScore} `) ; 
     
 
 
     //if (playerScore > computerScore) {
-   //     alert (`You win ${playerScore} to ${computerScore} !`)
-   // }else if (computerScore > playerScore) {
-     //   alert ( `You lose ${playerScore} to ${computerScore}`)
+      //  alert (`You win ${playerScore} to ${computerScore} !`)
+    //}else if (computerScore > playerScore) {
+      // alert ( `You lose ${playerScore} to ${computerScore}`)
     //}else{ 
-    //    alert (`The score is a tie. ${playerScore} - ${computerScore} ` )
-   // }
+      //  alert (`The score is a tie. ${playerScore} - ${computerScore} ` )
+   //}
 //}
 
-    //game() ;
+  //  game() ;
 
 
