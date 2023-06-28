bf8806780538c918564047964986e1300675a046
Ohm Patel <60417541+Ohm013@users.noreply.github.com>
2023-06-28 10:02:57 -0500

    made scoreboard visible at 0-0. Was only showing when one round of play completed before. Tried creating function to stop game when one side scores 5 but still working on it

--- javascript.js
+++ javascript.js
@@ -19,13 +19,15 @@ function getComputerChoice () { //Generates computers choice
     let choice = options[Math.floor(Math.random() * options.length)];
     return choice ;
 
-}   
+}
+
+score.textContent = "0 - 0" ; 
 
 function playRound (playerSelection, computerSelection) { //One round of the game
 
    
       computerSelection = getComputerChoice();
-        alert(`Computer chose ${computerSelection}`);  
+        alert(`Computer chose ${computerSelection}`);  //Make function that pops up a picture of computerchoice
 
 
     if ((playerSelection === "Rock" && computerSelection === "Paper" ) ||
@@ -33,32 +35,26 @@ function playRound (playerSelection, computerSelection) { //One round of the gam
        (playerSelection === "Paper" && computerSelection === "Scissors")) {
 
         words.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
-        score.textContent = `${playerScore} - ${computerScore}` ; 
         ++computerScore;
         
-    }
-    else if ((playerSelection === "Paper" && computerSelection === "Rock") ||
+    }else if ((playerSelection === "Paper" && computerSelection === "Rock") ||
             (playerSelection === "Rock" && computerSelection === "Scissors") ||
             (playerSelection === "Scissors" && computerSelection === "Paper")) {
        
         words.textContent = `You win! ${playerSelection} beats ${computerSelection}` ; 
-        score.textContent = `${playerScore} - ${computerScore}` ;
         ++playerScore;
 
+    }else{
+      words.textContent = "Try again! It's a tie";
     }
-    else{
-        words.textContent = "Try again! It's a tie";
-    }
-   
+    score.textContent = `${playerScore} - ${computerScore}` ;
 } 
-   
 
-    //if (playerScore > computerScore) {
-      //  alert (`You win ${playerScore} to ${computerScore} !`)
-    //}else if (computerScore > playerScore) {
-      // alert ( `You lose ${playerScore} to ${computerScore}`)
-    //}else{ 
-      //  alert (`The score is a tie. ${playerScore} - ${computerScore} ` )
-   //}
+
+
+
+//if 
+  //
+  //}
+
 //}
-
