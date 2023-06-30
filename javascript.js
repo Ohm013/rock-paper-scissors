97d808fa57214366d23a471a46f1878c615d543d
Ohm Patel <60417541+Ohm013@users.noreply.github.com>
2023-06-29 20:50:16 -0500

    querySelected restart button, added event listener to set score back to 0 immediately after clicked, not after a round being done

--- javascript.js
+++ javascript.js
@@ -1,9 +1,10 @@
 let playerScore = 0 ;
 let computerScore = 0 ;
-const buttons = document.querySelectorAll('button'); 
+const buttons = document.querySelectorAll('div.choices button'); 
 //const result = document.querySelector('result') ; 
 const words = document.querySelector('h3'); 
 const score = document.querySelector('h2');
+const restart = document.querySelector('#restart'); 
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
@@ -48,13 +49,33 @@ function playRound (playerSelection, computerSelection) { //One round of the gam
       words.textContent = "Try again! It's a tie";
     }
     score.textContent = `${playerScore} - ${computerScore}` ;
+    
 } 
+restart.addEventListener('click', startOver);
+
+function startOver () {
+
+   score.textContent = "0 - 0 " ; 
+
+  return score ; 
+
+}
 
 
-
-
-//if 
-  //
-  //}
+  
 
 //}
+
+
+// make a function to where game stops when one either side scores 5 
+
+
+
+  //if ((playerScore = 5) {
+   // words.textContent = "YOU WON! First to 5 points !"
+    
+    
+    
+   //}else if  (computerScore = 5)){
+     // words.textContent = "You lose :( Click the button below to try again"
+   //}
