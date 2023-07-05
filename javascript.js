2ce58f80f43384361728b4c9b5ace9e7971c9c4d
Ohm Patel <60417541+Ohm013@users.noreply.github.com>
2023-07-04 21:48:38 -0500

    querySelected player and compscore for scoreboard. replaces score variable with individual variables for player and comp score

--- javascript.js
+++ javascript.js
@@ -1,20 +1,18 @@
 let playerScore = 0 ;
 let computerScore = 0 ;
 const buttons = document.querySelectorAll('div.choices button'); 
-//const result = document.querySelector('result') ; 
 const words = document.querySelector('h3'); 
-const score = document.querySelector('h2');
+const pScore = document.querySelector('.playerScore');
+const cScore = document.querySelector('.computerScore');
 const restart = document.querySelector('#restart'); 
-const para = document.querySelector('p'); 
-//var rock = "https://cdn2.iconfinder.com/data/icons/hand-gestures-2-1/128/Rock-Hand-Game-Right-Gesture-Win-512.png";
-//var paper = "https://cdn-icons-png.flaticon.com/512/470/470827.png" ;
-//var scissors = "https://cdn1.iconfinder.com/data/icons/paper-rock-scissors/100/2-512.png" ; 
+const para = document.querySelector('.compChoice'); 
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
       playRound(button.value);
-
-    });
+      if ((playerScore >= 5) || (computerScore >=5)) {
+        return ;
+}});
     
 });     
 
@@ -26,7 +24,8 @@ function getComputerChoice () { //Generates computers choice
 
 }
 
-score.textContent = "0 - 0" ; 
+pScore.textContent = "0" ;
+cScore.textContent = "0";  
 
 function playRound (playerSelection, computerSelection) { //One round of the game
  
@@ -39,53 +38,38 @@ function playRound (playerSelection, computerSelection) { //One round of the gam
        (playerSelection === "Paper" && computerSelection === "Scissors")) {
         ++computerScore;
         if (computerScore === 5) {
-          words.textContent = "Game Over :( Press Play Again for rematch";
-          disableBtn();
+          words.textContent = "Game Over :( Press Play Again for rematch"; 
         }else { 
           words.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
       }     
     }else if ((playerSelection === "Paper" && computerSelection === "Rock") ||
           (playerSelection === "Rock" && computerSelection === "Scissors") ||
           (playerSelection === "Scissors" && computerSelection === "Paper")) {
-            ++playerScore;     
+            ++playerScore;   
             if (playerScore === 5) {
               words.textContent = "YOU WIN!!";
-              disableBtn() ;
             }else {
               words.textContent = `You win! ${playerSelection} beats ${computerSelection}` ; 
         }      
     }else{
       words.textContent = "Try again! It's a tie";
     }
-    score.textContent = `${playerScore} - ${computerScore}` ;
-}    
-  
-restart.addEventListener('click', startOver) ; 
-
-
-function reAppear () {
-  if ((playerScore < 5) || (computerScore < 5)) {
-    restart.style.visibility = 'hidden';
-  }else {
-    restart.style.visibility = 'visible';
+   pScore.textContent = `${playerScore}` ;
+   cScore.textContent = `${computerScore}`;
   }
 
-}
+
+restart.addEventListener('click', startOver) ; 
+
 function startOver () {
-  score.textContent = "0 - 0 " ; 
+  pScore.textContent = "0" ; 
+  cScore.textContent = "0" ;
   playerScore = 0;
   computerScore = 0; 
   return score ; 
 
 }
 
-function disableBtn () {
-  return playerScore ; 
-  return computerScore ;
- if ((playerScore === 5) || (computerScore === 5 )) {
-  buttons.disbaled = true ;
- }else {
-  buttons.disbaled = false ;
-}}
+
 
 
